10916cb297988d16dc7f73a0eea462e0d56d6bd3
Rogerio <rrgmax@bol.com.br>
2019-02-20 00:07:35 -0300

    Update Product class into models.py

--- src/products/models.py
+++ src/products/models.py
@@ -3,6 +3,9 @@ from django.db import models
 # Create your models here.
 class Product(models.Model):
     title = models.CharField(max_length=200)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=None, decimal_places=None)
+    
     
 
     def __str__(self):
