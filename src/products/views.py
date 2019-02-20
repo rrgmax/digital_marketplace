ca3b7c72a1137e87d4d4c53bf60430222546cc76
Rogerio <rrgmax@bol.com.br>
2019-02-20 19:52:21 -0300

    Set detail_view and list_view into views.py file

--- src/products/views.py
+++ src/products/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
 
 # Create your views here.
+def detail_view(request):
+    template = "detail_view.html"
+    context = {}
+    return render(request, template, context)
+
+def list_view(request):
+    template = "detail_view.html"
+    context = {}
+    return render(request, template, context)
