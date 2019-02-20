8fa3c425ebf79658043e30945a9f49aec58df502
Rogerio <rrgmax@bol.com.br>
2019-02-20 01:51:03 -0300

    Remove db.sqlite3, update models.py and run migrations.

--- src/products/models.py
+++ src/products/models.py
@@ -3,7 +3,7 @@ from django.db import models
 # Create your models here.
 class Product(models.Model):
     title = models.CharField(max_length=200)
-    description = models.TextField(null=True)
+    
     price = models.DecimalField(max_digits=100, decimal_places=2, default=9.99)
 
     
