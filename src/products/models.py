eb885451da842e17fcff959b59eef6fc24b7d1a8
Rogerio <rrgmax@bol.com.br>
2019-02-20 02:15:01 -0300

    Update models,py and set Product into admin.py file

--- src/products/models.py
+++ src/products/models.py
@@ -3,10 +3,10 @@ from django.db import models
 # Create your models here.
 class Product(models.Model):
     title = models.CharField(max_length=200)
-    
-    price = models.DecimalField(max_digits=100, decimal_places=2, default=9.99)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=100, decimal_places=2, default=99.99)
+    sale_price = models.DecimalField(max_digits=100, decimal_places=2, default=99.99, null=True, blank=True)
 
-    
-
+ 
     def __str__(self):
         return self.title
