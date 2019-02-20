f32f7d90815321ad73424295c424deeb38d274cc
Rogerio <rrgmax@bol.com.br>
2019-02-20 00:13:07 -0300

    Update models.py and run migrations

--- src/products/models.py
+++ src/products/models.py
@@ -3,9 +3,9 @@ from django.db import models
 # Create your models here.
 class Product(models.Model):
     title = models.CharField(max_length=200)
-    description = models.TextField()
-    price = models.DecimalField(max_digits=None, decimal_places=None)
-    
+    description = models.TextField(null=True)
+    price = models.DecimalField(max_digits=100, decimal_places=2, default=9.99)
+
     
 
     def __str__(self):
