59745a8fd4ce1a07b57a9bec3b09ee09bb621bf0
Rogerio <rrgmax@bol.com.br>
2019-02-20 02:58:03 -0300

    Set admin.py file

--- src/products/admin.py
+++ src/products/admin.py
@@ -3,4 +3,12 @@ from django.contrib import admin
 # Register your models here.
 from .models import Product
 
-admin.site.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ["__str__", "description", "price", "sale_price"]
+    search_fields = ["description", "title"]
+    list_filter = ["price"]
+    list_editable = ["sale_price"]
+    class Meta:
+        model = Product
+
+admin.site.register(Product, ProductAdmin)
