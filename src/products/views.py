6acf6e35131ffab9a9324c7ad42011e6371e3682
Rogerio <rrgmax@bol.com.br>
2019-02-20 22:53:12 -0300

    Insert user.is_authenticated in the detail_view into views.py

--- src/products/views.py
+++ src/products/views.py
@@ -4,15 +4,22 @@ from .models import Product
 
 # Create your views here.
 def detail_view(request):
-    product = Product.objects.all().first()
-    template = "detail_view.html"
-    context = {
-        "title": "Hello Again",
-        "product": product
-    }
+    if request.user.is_authenticated:
+        product = Product.objects.all().first()
+        template = "detail_view.html"
+        context = {
+            "title": "Hello Again",
+            "object": product
+        }
+    else:    
+        template = "not_found.html"
+        context = {}
     return render(request, template, context)
 
 def list_view(request):
-    template = "detail_view.html"
-    context = {}
+    queryset = Product.objects.all()
+    template = "list_view.html"
+    context = {
+        "queryset": queryset
+    }
     return render(request, template, context)
