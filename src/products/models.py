b6b7963d4b4d65a97e3f416282b7fcf650aab5a2
Rogerio <rrgmax@bol.com.br>
2019-02-21 16:28:25 -0300

    Insert signals to slugify title into models.py file

--- src/products/models.py
+++ src/products/models.py
@@ -1,9 +1,11 @@
 from django.db import models
+from django.db.models.signals import pre_save, post_save
+from django.utils.text import slugify
 
 # Create your models here.
 class Product(models.Model):
     title = models.CharField(max_length=200)
-    slug = models.SlugField(unique=True)
+    slug = models.SlugField(default='slug-field')
     description = models.TextField(default=None)
     price = models.DecimalField(max_digits=100, decimal_places=2, default=99.99)
     sale_price = models.DecimalField(max_digits=100, decimal_places=2, default=99.99, null=True, blank=True)
@@ -11,3 +13,9 @@ class Product(models.Model):
  
     def __str__(self):
         return self.title
+
+def product_pre_save_reciever(sender, instance, *args, **kwargs):
+    if not instance.slug:
+        instance.slug = slugify(instance.title)
+
+pre_save.connect(product_pre_save_reciever, sender=Product)
