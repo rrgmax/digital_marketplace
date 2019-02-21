f4221998a2d2dac2a9514574df7082d79060c2c8
Rogerio <rrgmax@bol.com.br>
2019-02-21 17:37:22 -0300

    Define create_view and set into urls.py. Create forms.py and create_view.html files

--- src/digitalmarket/urls.py
+++ src/digitalmarket/urls.py
@@ -20,6 +20,7 @@ from products import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    re_path('create/', views.create_view, name="create_view"),
     re_path('detail/(?P<object_id>\d+)/$', views.detail_view, name="detail_view"),
     re_path('detail/(?P<slug>[\w-]+)/$', views.detail_slug_view, name="detail_slug_view"),
     re_path('list/', views.list_view, name="list_view"),
--- src/products/views.py
+++ src/products/views.py
@@ -1,10 +1,38 @@
+from django.http import Http404
 from django.shortcuts import render, get_object_or_404
 
+from .forms import ProductAddForm
 from .models import Product
 
-# Create your views here.
+
+def create_view(request):
+    form = ProductAddForm(request.POST or None)
+    # if request.method == "POST":
+    #     print request.POST.get("price")
+    if form.is_valid():
+        data = form.cleaned_data
+        title = data.get("title")
+        description = data.get("description")
+        price = data.get("price")
+        new_obj = Product()
+        new_obj.title = title
+        new_obj.description = description
+        new_obj.price = price
+        new_obj.save()
+    template = "create_view.html"
+    context = {
+            "form":form,
+        }
+    return render(request, template, context)    
+
+
+
 def detail_slug_view(request, slug=None):
-    product = get_object_or_404(Product, slug=slug)
+    product = Product.objects.get(slug=slug)
+    try:
+        product = get_object_or_404(Product, slug=slug)
+    except Product.MultupleObjectsReturned:
+        product = Product.objects.filter(slug=slug).order_by("title").first()    
     template = "detail_view.html"
     context = {
         "object": product
