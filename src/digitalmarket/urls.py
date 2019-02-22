ff558aca0f88df06f8fe276e8d6743ccdd738f0a
Rogerio <rrgmax@bol.com.br>
2019-02-21 23:28:40 -0300

    Define update_view into views.py and set it up into urls.py

--- src/digitalmarket/urls.py
+++ src/digitalmarket/urls.py
@@ -1,18 +1,3 @@
-"""digitalmarket URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/2.1/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
 from django.urls import path, re_path
 
@@ -21,6 +6,7 @@ from products import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     re_path('create/', views.create_view, name="create_view"),
+    re_path('detail/(?P<object_id>\d+)/edit/$', views.update_view, name="update_view"),
     re_path('detail/(?P<object_id>\d+)/$', views.detail_view, name="detail_view"),
     re_path('detail/(?P<slug>[\w-]+)/$', views.detail_slug_view, name="detail_slug_view"),
     re_path('list/', views.list_view, name="list_view"),
