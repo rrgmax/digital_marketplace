f76b0776e5098825eaa0e9d73ee8c5e663dc3bed
Rogerio <rrgmax@bol.com.br>
2019-02-21 21:07:43 -0300

    Update forms.py and views.py in order to render placeholder and title in the forms fields

--- src/products/forms.py
+++ src/products/forms.py
@@ -1,5 +1,7 @@
 from django import forms
 
+from .models import Product
+
 PUBLISH_CHOICES = {
     ('', ""),
     ('publish', "Publish"),
@@ -38,3 +40,48 @@ class ProductAddForm(forms.Form):
             return title
         else:
             raise forms.ValidationError("Title must be greater than 3 characters long.")    
+
+class ProductModelForm(forms.ModelForm):
+    publish = forms.ChoiceField(choices=PUBLISH_CHOICES, required=False)
+    # description = forms.CharField(widget=forms.Textarea(
+    #         attrs={
+    #             "class": "my-custom-class",
+    #             "placeholder": "Description",
+    #             "some-attr": "this",
+    #         }
+    # ))          
+    class Meta:
+        model = Product
+        fields = [
+            "title",
+            "description",
+            "price",
+        ]
+        widgets = {
+            "description": forms.Textarea(
+                attrs={
+                    "placeholder": "New Description"
+                }
+            ),
+            "title": forms.TextInput(
+                attrs= {
+                    "placeholder": "Title"
+                }
+            )
+        }
+
+    def clean_price(self):
+        price = self.cleaned_data.get("price")
+        if price <= 1.00:
+            raise forms.ValidationError("Price must be greater than $1.00")
+        elif price >= 99.99:
+            raise forms.ValidationError("Price must be less than $100.00")
+        return price    
+
+    def clean_title(self):
+        title = self.cleaned_data.get("title")
+        if len(title) > 3:
+            return title
+        else:
+            raise forms.ValidationError("Title must be greater than 3 characters long.")        
+            
