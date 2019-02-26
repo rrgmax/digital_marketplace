4b6710c875e1a0f6a7615fc7012cfdbbf8233f02
Rogerio <rrgmax@bol.com.br>
2019-02-26 05:12:46 -0300

    Create static folder and set static files into settings.py

--- src/digitalmarket/settings.py
+++ src/digitalmarket/settings.py
@@ -119,3 +119,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, "static"),
+)
+
+STATIC_ROOT = os.path.join(os.path.dirname(BASE_DIR), "static_cdn", "staticfiles")
+
+MEDIA_URL = '/media/'
+
+MEDIA_ROOT = os.path.join(os.path.dirname(BASE_DIR), "static_cdn", "media")
