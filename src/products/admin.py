5ac96a407f16de3962acbe645dbebb666c0d37df
Rogerio <rrgmax@bol.com.br>
2019-02-27 20:16:13 -0300

    Define ThumbnailInline class into admin.py file

--- src/products/admin.py
+++ src/products/admin.py
@@ -3,16 +3,19 @@ from django.contrib import admin
 # Register your models here.
 from .models import Product, MyProducts, Thumbnail
 
+class ThumbnailInline(admin.TabularInline):
+    extra = 1
+    model = Thumbnail
+
 class ProductAdmin(admin.ModelAdmin):
+    inlines = [ThumbnailInline]
     list_display = ["__str__", "description", "price", "sale_price"]
     search_fields = ["description", "title"]
-    list_filter = ["price"]
-    list_editable = ["price","sale_price"]
+    list_filter = ["price", "sale_price"]
+    list_editable = ["sale_price"]
     class Meta:
         model = Product
 
 admin.site.register(Product, ProductAdmin)
-
-admin.site.register(Thumbnail)
 
 admin.site.register(MyProducts)
